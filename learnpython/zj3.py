28130ab3d0ba331c5b563d37fb451bf20ccfd0eb
XSBlingB <brave_shine_xs@163.com>
2019-03-14 15:09:46 +0800

    new a zj3.py in learnpython folder in branch learnpython

---
+++ learnpython/zj3.py
@@ -0,0 +1,95 @@
+d = {'a': 1, 'b': 2, 'c': 3}
+for key in d:
+    print(key)
+
+for ch in 'ABC': #字符串也属于可迭代对象
+    print(ch)
+
+
+for i, value in enumerate(['A', 'B', 'C']):
+    print(i, value)
+
+def findMinAndMax(L):
+    if L == []:
+        return (None,None)
+    else:
+        min = L[0]
+        max = L[0]
+        for i in L:
+            if min > i:
+                min = i
+            elif max < i:
+                max = i
+        return (min,max)  
+    # 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')
+
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2=[s.lower() for s in L1 if isinstance(s,str) is True]
+# 测试:
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+#杨辉三角
+def triangles():    
+    L = [1]
+    while True:  
+        yield L              
+        L = [1]+[L[i] + L[i+1] for i in range(len(L)-1)]+[1]
+
+# 期待输出:
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# [1, 5, 10, 10, 5, 1]
+# [1, 6, 15, 20, 15, 6, 1]
+# [1, 7, 21, 35, 35, 21, 7, 1]
+# [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
+#reduce实现1，3，5，7，9累加
+from functools import reduce
+def add(x, y):
+    return x + y
+print(reduce(add, [1, 3, 5, 7, 9]))
+#reduce实现1，3，5，9转化为13579
+from functools import reduce
+def fn(x, y):
+    return x * 10 + y
+print('13579:',reduce(fn, [1, 3, 5, 7, 9]))
